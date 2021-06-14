6e68f9579b9a5e9db8b6c43ac890a8d52e554574
kyra whitman <examplew.com>
2021-06-14 14:51:14 -0500

    print statement complete

---
+++ ToDoList3/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  ToDoList3
+//
+//  Created by Kyra Whitman on 6/14/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        print("doex this work")
+    }
+
+
+}
+
